332a5dc7c1d147fec4eb116f9cc108d6d319bf51
CoderMungan <codermungan@gmail.com>
2024-06-02 14:45:13 +0300

    feat(apropospriate): update background

--- lua/plugins/apropospriate.lua
+++ lua/plugins/apropospriate.lua
@@ -1,9 +1,11 @@
 return {
   "miffi/apropospriate.nvim",
   lazy = false,
-  priority = 1000, -- Ensure that the colorscheme loads before any other plugins
+  priority = 1000, -- Renk şemasının diğer eklentilerden önce yüklenmesini sağlar
   config = function()
     vim.o.termguicolors = true
     vim.cmd.colorscheme("apropospriate")
+    vim.o.background = "dark"
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
   end,
 }
